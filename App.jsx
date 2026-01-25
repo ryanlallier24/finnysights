97c9dc3ebc56f309d2102f448d49a2e943fd5d26
ryanlallier24 <ryanlallier@gmail.com>
2026-01-24 22:37:49 -0500

    Update App.jsx

--- App.jsx
+++ App.jsx
@@ -1,26 +1,11 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-
-// Import from flat structure (all files in root)
-import FinnysightsLanding from './finnysights-landing';
-import FinnysightsApp from './finnysights-app';
-import FinnysightsDashboard from './finnysights-dashboard';
-import FinnysightsApiDocs from './finnysights-api-docs';
-import FinnysightsStockDetail from './finnysights-stock-detail';
+import FinnysightsLanding from './finnysights-landing.jsx';
 
 function App() {
   return (
     <Router>
       <Routes>
-        {/* Public routes */}
         <Route path="/" element={<FinnysightsLanding />} />
-        <Route path="/docs" element={<FinnysightsApiDocs />} />
-        
-        {/* App routes */}
-        <Route path="/app" element={<FinnysightsApp />} />
-        <Route path="/dashboard" element={<FinnysightsDashboard />} />
-        <Route path="/stock/:symbol" element={<FinnysightsStockDetail />} />
-        
-        {/* 404 fallback */}
         <Route path="*" element={<FinnysightsLanding />} />
       </Routes>
     </Router>
